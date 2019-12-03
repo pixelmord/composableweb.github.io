3f260a46e7fd42575723547407856a83053528aa
Andreas Sahle <andreas.sahle@gmail.com>
2019-12-03 12:18:46 +0100

    chore: starting point

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ComposableWeb`,
+    description: `Promoting Composability for the web`,
+    author: `@pixelmord`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

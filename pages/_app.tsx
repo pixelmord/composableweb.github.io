d3b8f3315a4d50cc7bd603eb9a0d1446d21fa8f7
Andreas Adam <andreas.sahle@gmail.com>
2020-08-21 16:23:51 +0200

    fix: head to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Head } from 'next/document';
+import Head from 'next/head';
 import { ThemeProvider } from 'theme-ui';
 import theme from '../styles';
 import PageLayout from '~components/PageLayout';
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
+  <>
     <Head>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
     </Head>
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+    </ThemeProvider>
+  </>
 );
 
 export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,20 @@
-import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-
           <meta content="#ffffff" name="theme-color" />
           <meta content="#ffffff" name="msapplication-TileColor" />
           <meta content="/static/browserconfig.xml" name="msapplication-config" />
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-          {/* <link href="/static/site.webmanifest" rel="manifest" /> */}
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
-          <link rel="preconnect" href="https://cdn.usefathom.com" crossOrigin="" /> */}
           <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
           <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
           <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }

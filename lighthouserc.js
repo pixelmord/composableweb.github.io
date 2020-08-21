3b3a306c4e2e2db88533348e90eeb38537f4983f
Andreas Adam <andreas.sahle@gmail.com>
2020-08-21 17:39:09 +0200

    chore: stricter testing for perf

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,7 +7,8 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 1 }],
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
         'categories:accessibility': ['error', { minScore: 1 }]
       }
     },
